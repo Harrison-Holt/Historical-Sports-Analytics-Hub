a0615de50d2748c7ed34ec4cc0cefcf7fa26a6aa
Harrison-Holt <hholt2901@gmail.com>
2024-12-03 12:00:18 -0500

    added React Router Dom into App.jsx

--- Historical-Sports-Analytics-Hub/src/App.jsx
+++ Historical-Sports-Analytics-Hub/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Route, Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+
 import Register from './Frontend/Register.jsx'
 
 function App() {
